b52c59bb1af1b1762ab74979c8d4a01bb15b5cea
Alan Ayala <aalanaayala@live.com>
2025-04-08 23:54:31 -0600

    Se arregló la vista de home

--- core/app/view/home-view.php
+++ core/app/view/home-view.php
@@ -30,96 +30,109 @@ foreach($configs as $conf) {
                     <h3 class="card-title">Dashboard</h3>
                 </div>
                 <div class="card-body">
-                    <!-- Contenido del dashboard -->
+                    <div class="row">
+                        <div class="col-6 col-lg-3">
+                            <div class="card">
+                                <div class="card-body p-3 d-flex align-items-center">
+                                    <div class="bg-primary text-white p-3 me-3">
+                                        <svg class="icon icon-xl">
+                                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-smile"></use>
+                                        </svg>
+                                    </div>
+                                    <div>
+                                        <div class="fs-6 fw-semibold text-primary"><?php echo count(ProductData::getAll());?></div>
+                                        <div class="text-medium-emphasis text-uppercase fw-semibold small">INVENTARIO</div>
+                                    </div>
+                                </div>
+                                <div class="card-footer px-3 py-2">
+                                    <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href="./?view=inventary">
+                                        <span class="small fw-semibold">IR A INVENTARIO</span>
+                                        <svg class="icon">
+                                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-chevron-right"></use>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <?php if($clients_enabled): ?>
+                        <div class="col-6 col-lg-3">
+                            <div class="card">
+                                <div class="card-body p-3 d-flex align-items-center">
+                                    <div class="bg-info text-white p-3 me-3">
+                                        <svg class="icon icon-xl">
+                                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-people"></use>
+                                        </svg>
+                                    </div>
+                                    <div>
+                                        <div class="fs-6 fw-semibold text-info"><?php echo count(PersonData::getClients());?></div>
+                                        <div class="text-medium-emphasis text-uppercase fw-semibold small">CLIENTES</div>
+                                    </div>
+                                </div>
+                                <div class="card-footer px-3 py-2">
+                                    <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href="./?view=clients">
+                                        <span class="small fw-semibold">IR A CLIENTES</span>
+                                        <svg class="icon">
+                                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-chevron-right"></use>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <?php endif; ?>
+                        <?php if($providers_enabled): ?>
+                        <div class="col-6 col-lg-3">
+                            <div class="card">
+                                <div class="card-body p-3 d-flex align-items-center">
+                                    <div class="bg-warning text-white p-3 me-3">
+                                        <svg class="icon icon-xl">
+                                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-truck"></use>
+                                        </svg>
+                                    </div>
+                                    <div>
+                                        <div class="fs-6 fw-semibold text-warning"><?php echo count(PersonData::getProviders());?></div>
+                                        <div class="text-medium-emphasis text-uppercase fw-semibold small">PROVEEDORES</div>
+                                    </div>
+                                </div>
+                                <div class="card-footer px-3 py-2">
+                                    <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href="./?view=providers">
+                                        <span class="small fw-semibold">IR A PROVEEDORES</span>
+                                        <svg class="icon">
+                                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-chevron-right"></use>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <?php endif; ?>
+                        <div class="col-6 col-lg-3">
+                            <div class="card">
+                                <div class="card-body p-3 d-flex align-items-center">
+                                    <div class="bg-danger text-white p-3 me-3">
+                                        <svg class="icon icon-xl">
+                                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
+                                        </svg>
+                                    </div>
+                                    <div>
+                                        <div class="fs-6 fw-semibold text-danger"><?php echo count(CategoryData::getAll());?></div>
+                                        <div class="text-medium-emphasis text-uppercase fw-semibold small">CATEGORIAS</div>
+                                    </div>
+                                </div>
+                                <div class="card-footer px-3 py-2">
+                                    <a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href="./?view=categories">
+                                        <span class="small fw-semibold">IR A CATEGORIAS</span>
+                                        <svg class="icon">
+                                            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-chevron-right"></use>
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-                    <div class="row">
-                      <div class="col-6 col-lg-3">
-                        <div class="card">
-                          <div class="card-body p-3 d-flex align-items-center">
-                            <div class="bg-primary text-white p-3 me-3">
-                              <svg class="icon icon-xl">
-                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-smile"></use>
-                              </svg>
-                            </div>
-                            <div>
-                              <div class="fs-6 fw-semibold text-primary"><?php echo count(ProductData::getAll());?></div>
-                              <div class="text-medium-emphasis text-uppercase fw-semibold small">INVENTARIO</div>
-                            </div>
-                          </div>
-                          <div class="card-footer px-3 py-2"><a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href="./?view=inventary"><span class="small fw-semibold">IR A INVENTARIO</span>
-                              <svg class="icon">
-                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-chevron-right"></use>
-                              </svg></a></div>
-                        </div>
-                      </div>
-                      <!-- /.col-->
-                      <?php if($clients_enabled): ?>
-                      <div class="col-6 col-lg-3">
-                        <div class="card">
-                          <div class="card-body p-3 d-flex align-items-center">
-                            <div class="bg-info text-white p-3 me-3">
-                              <svg class="icon icon-xl">
-                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-people"></use>
-                              </svg>
-                            </div>
-                            <div>
-                              <div class="fs-6 fw-semibold text-info"><?php echo count(PersonData::getClients());?></div>
-                              <div class="text-medium-emphasis text-uppercase fw-semibold small">IR A CLIENTES</div>
-                            </div>
-                          </div>
-                          <div class="card-footer px-3 py-2"><a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href="./?view=clients"><span class="small fw-semibold">IR A CLIENTES</span>
-                              <svg class="icon">
-                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-chevron-right"></use>
-                              </svg></a></div>
-                        </div>
-                      </div>
-                      <?php endif; ?>
-                      <?php if($providers_enabled): ?>
-                      <div class="col-6 col-lg-3">
-                        <div class="card">
-                          <div class="card-body p-3 d-flex align-items-center">
-                            <div class="bg-warning text-white p-3 me-3">
-                              <svg class="icon icon-xl">
-                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-truck"></use>
-                              </svg>
-                            </div>
-                            <div>
-                              <div class="fs-6 fw-semibold text-warning"><?php echo count(PersonData::getProviders());?></div>
-                              <div class="text-medium-emphasis text-uppercase fw-semibold small">IR A PROVEEDORES</div>
-                            </div>
-                          </div>
-                          <div class="card-footer px-3 py-2"><a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href="./?view=providers"><span class="small fw-semibold">IR A PROVEEDORES</span>
-                              <svg class="icon">
-                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-chevron-right"></use>
-                              </svg></a></div>
-                        </div>
-                      </div>
-                      <?php endif; ?>
-                      <div class="col-6 col-lg-3">
-                        <div class="card">
-                          <div class="card-body p-3 d-flex align-items-center">
-                            <div class="bg-danger text-white p-3 me-3">
-                              <svg class="icon icon-xl">
-                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
-                              </svg>
-                            </div>
-                            <div>
-                              <div class="fs-6 fw-semibold text-danger"><?php echo count(CategoryData::getAll());?></div>
-                              <div class="text-medium-emphasis text-uppercase fw-semibold small">CATEGORIAS</div>
-                            </div>
-                          </div>
-                          <div class="card-footer px-3 py-2"><a class="btn-block text-medium-emphasis d-flex justify-content-between align-items-center" href="./?view=categories"><span class="small fw-semibold">IR A CATEGORIAS</span>
-                              <svg class="icon">
-                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-chevron-right"></use>
-                              </svg></a></div>
-                        </div>
-                      </div>
-                    </div>
 
 <br>
 <div class="row">

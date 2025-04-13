ece5e7a020b53356b5623b27b0a0d3e552e664ec
Alan Ayala <theogchsn1@outlook.com>
2025-04-12 19:05:52 -0600

    Se arreglo la vista de editproduct

--- core/app/view/editproduct-view.php
+++ core/app/view/editproduct-view.php
@@ -18,119 +18,159 @@ if($product!=null):
     <div class="card-body">
 
 
-		<form class="form-horizontal" method="post" id="addproduct" enctype="multipart/form-data" action="index.php?view=updateproduct<?php 
-            $filter_params = array("category_id", "availability", "size", "date_filter", "search", "limit", "jerseyType", "page");
-            foreach($filter_params as $param) {
-                if(isset($_GET[$param]) && $_GET[$param] != "") {
-                    echo "&" . $param . "=" . urlencode($_GET[$param]);
-                }
-            }
-        ?>" role="form">
-
-  <div class="form-group">
-    <label for="inputEmail1" class="col-lg-3 control-label">Imagen*</label>
-    <div class="col-md-8">
-      <input type="file" name="image" id="name" placeholder="">
-<?php if($product->image!=""):?>
-  <br>
-        <img src="storage/products/<?php echo $product->image;?>" class="img-fluid">
-
-<?php endif;?>
-    </div>
-  </div>
-
-  <?php
-  // Agregar campos ocultos para los parámetros de filtro
-  $filter_params = array("category_id", "availability", "size", "date_filter", "search", "limit", "jerseyType", "page");
-  foreach($filter_params as $param) {
-      if(isset($_GET[$param]) && $_GET[$param] != "") {
-          echo '<input type="hidden" name="' . $param . '" value="' . htmlspecialchars($_GET[$param]) . '">';
-      }
-  }
-  ?>
-
-  <div class="form-group">
-    <label for="inputEmail1" class="col-lg-3 control-label">Codigo de barras*</label>
-    <div class="col-md-8">
-      <input type="text" name="barcode" class="form-control" id="barcode" value="<?php echo $product->barcode; ?>" placeholder="Codigo de barras del Producto">
-    </div>
-  </div>
-    <div class="form-group">
-    <label for="inputEmail1" class="col-lg-3 control-label">Nombre*</label>
-    <div class="col-md-8">
-      <input type="text" name="name" class="form-control" id="name" value="<?php echo $product->name; ?>" placeholder="Nombre del Producto">
-    </div>
-  </div>
-    <div class="form-group">
-    <label for="inputEmail1" class="col-lg-3 control-label">Categoria</label>
-    <div class="col-md-8">
-    <select name="category_id" class="form-control">
-    <option value="">-- NINGUNA --</option>
-    <?php foreach($categories as $category):?>
-      <option value="<?php echo $category->id;?>" <?php if($product->category_id!=null&& $product->category_id==$category->id){ echo "selected";}?>><?php echo $category->name;?></option>
-    <?php endforeach;?>
-      </select>    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputEmail1" class="col-lg-3 control-label">Descripcion</label>
-    <div class="col-md-8">
-      <textarea name="description" class="form-control" id="description" placeholder="Descripcion del Producto"><?php echo $product->description;?></textarea>
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="inputEmail1" class="col-lg-3 control-label">Precio de Entrada*</label>
-    <div class="col-md-8">
-      <input type="text" name="price_in" class="form-control" value="<?php echo $product->price_in; ?>" id="price_in" placeholder="Precio de entrada">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputEmail1" class="col-lg-3 control-label">Precio de Salida*</label>
-    <div class="col-md-8">
-      <input type="text" name="price_out" class="form-control" id="price_out" value="<?php echo $product->price_out; ?>" placeholder="Precio de salida">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputEmail1" class="col-lg-3 control-label">Unidad*</label>
-    <div class="col-md-8">
-      <input type="text" name="unit" class="form-control" id="unit" value="<?php echo $product->unit; ?>" placeholder="Unidad del Producto">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputEmail1" class="col-lg-3 control-label">Presentacion</label>
-    <div class="col-md-8">
-      <input type="text" name="presentation" class="form-control" id="inputEmail1" value="<?php echo $product->presentation; ?>" placeholder="Presentacion del Producto">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputEmail1" class="col-lg-3 control-label">Minima en inventario:</label>
-    <div class="col-md-8">
-      <input type="text" name="inventary_min" class="form-control" value="<?php echo $product->inventary_min;?>" id="inputEmail1" placeholder="Minima en Inventario (Default 10)">
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="inputEmail1" class="col-lg-3 control-label" >Esta activo</label>
-    <div class="col-md-8">
-<div class="checkbox">
-    <label>
-      <input type="checkbox" name="is_active" <?php if($product->is_active){ echo "checked";}?>> 
-    </label>
-  </div>
-    </div>
-  </div>
-
-  <div class="form-group">
-    <div class="col-lg-offset-3 col-lg-8">
-    <input type="hidden" name="product_id" value="<?php echo $product->id; ?>">
-      <button type="submit" class="btn btn-success">Actualizar Producto</button>
-    </div>
-  </div>
-</form>
+		<form method="post" action="index.php?view=updateproduct" enctype="multipart/form-data">
+			<div class="row">
+				<div class="col-md-8">
+					<input type="hidden" name="product_id" value="<?php echo $product->id; ?>">
+					<div class="form-group">
+						<label for="name">Nombre*</label>
+						<input type="text" name="name" value="<?php echo $product->name; ?>" class="form-control" id="name" placeholder="Nombre del producto">
+					</div>
+					<div class="form-group">
+						<label for="barcode">Código de Barras</label>
+						<input type="text" name="barcode" value="<?php echo $product->barcode; ?>" class="form-control" id="barcode" placeholder="Código de barras">
+					</div>
+					<div class="form-group">
+						<label for="description">Descripción</label>
+						<textarea name="description" class="form-control" id="description" placeholder="Descripción del producto"><?php echo $product->description; ?></textarea>
+					</div>
+					<div class="form-group">
+						<label for="presentation">Presentación</label>
+						<input type="text" name="presentation" value="<?php echo $product->presentation; ?>" class="form-control" id="presentation" placeholder="Presentación">
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="form-group">
+						<label for="image">Imagen</label>
+						<input type="file" name="image" id="image" class="form-control">
+						<?php if($product->image!=""):?>
+							<img src="storage/products/<?php echo $product->image; ?>" class="img-responsive" style="width:100px;">
+						<?php endif;?>
+					</div>
+					<div class="form-group">
+						<label for="category_id">Categoría</label>
+						<select name="category_id" id="category_id" class="form-control">
+							<option value="">-- SELECCIONE --</option>
+							<?php foreach($categories as $cat):?>
+								<option value="<?php echo $cat->id; ?>" <?php if($product->category_id==$cat->id){ echo "selected"; }?>><?php echo $cat->name; ?></option>
+							<?php endforeach;?>
+						</select>
+					</div>
+					<div class="form-group" id="jersey_type_group" style="display:none;">
+						<label for="tipo_jersey">Tipo de Jersey</label>
+						<select name="tipo_jersey" id="tipo_jersey" class="form-control">
+							<option value="">-- SELECCIONE --</option>
+							<option value="adulto" <?php if($product->jersey_type=="adulto"){ echo "selected"; }?>>Adulto</option>
+							<option value="niño" <?php if($product->jersey_type=="niño"){ echo "selected"; }?>>Niño</option>
+							<option value="dama" <?php if($product->jersey_type=="dama"){ echo "selected"; }?>>Dama</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="price_in">Precio de Entrada*</label>
+						<input type="text" name="price_in" value="<?php echo $product->price_in; ?>" class="form-control" id="price_in" placeholder="Precio de entrada">
+					</div>
+					<div class="form-group">
+						<label for="price_out">Precio de Salida*</label>
+						<input type="text" name="price_out" value="<?php echo $product->price_out; ?>" class="form-control" id="price_out" placeholder="Precio de salida">
+					</div>
+					<div class="form-group">
+						<label for="unit">Unidad*</label>
+						<input type="text" name="unit" value="<?php echo $product->unit; ?>" class="form-control" id="unit" placeholder="Unidad">
+					</div>
+					<div class="form-group">
+						<label for="inventary_min">Inventario Minimo*</label>
+						<input type="text" name="inventary_min" value="<?php echo $product->inventary_min; ?>" class="form-control" id="inventary_min" placeholder="Inventario Minimo">
+					</div>
+					<div class="form-group">
+						<div class="checkbox">
+							<label>
+								<input type="checkbox" name="is_active" <?php if($product->is_active){ echo "checked"; }?>> Activo
+							</label>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<button type="submit" class="btn btn-success">Actualizar</button>
+				</div>
+			</div>
+		</form>
     </div>
 </div>
 
 <br><br>
 	</div>
 </div>
+
+<script>
+	$(document).ready(function(){
+		// Mostrar/ocultar tipo de jersey según la categoría
+		$("#category_id").change(function(){
+			var category_id = $(this).val();
+			if(category_id){
+				$.get("index.php?action=getcategory&id="+category_id,function(data){
+					if(data.name.toLowerCase() == "jersey"){
+						$("#jersey_type_group").show();
+					}else{
+						$("#jersey_type_group").hide();
+					}
+				},"json");
+			}else{
+				$("#jersey_type_group").hide();
+			}
+		});
+
+		// Verificar categoría al cargar la página
+		var category_id = $("#category_id").val();
+		if(category_id){
+			$.get("index.php?action=getcategory&id="+category_id,function(data){
+				if(data.name.toLowerCase() == "jersey"){
+					$("#jersey_type_group").show();
+				}else{
+					$("#jersey_type_group").hide();
+				}
+			},"json");
+		}
+
+		// Modificar el formulario para que envíe todos los campos excepto la imagen si no se modificó
+		$("form").on("submit", function(e) {
+			e.preventDefault();
+			var formData = new FormData();
+			
+			// Agregar todos los campos del formulario excepto la imagen
+			$("input, select, textarea").each(function() {
+				var name = $(this).attr("name");
+				if (name && !$(this).is(":file")) {
+					if ($(this).attr("type") === "checkbox") {
+						formData.append(name, $(this).is(":checked") ? "1" : "0");
+					} else {
+						formData.append(name, $(this).val() || "");
+					}
+				}
+			});
+
+			// Agregar la imagen solo si se seleccionó una nueva
+			var imageInput = $("#image")[0];
+			if (imageInput.files.length > 0) {
+				formData.append("image", imageInput.files[0]);
+			}
+
+			// Enviar el formulario
+			$.ajax({
+				url: "index.php?view=updateproduct",
+				type: "POST",
+				data: formData,
+				processData: false,
+				contentType: false,
+				success: function(response) {
+					window.location.href = "index.php?view=inventary";
+				},
+				error: function(xhr, status, error) {
+					alert("Error al actualizar el producto: " + error);
+				}
+			});
+		});
+	});
+</script>
 <?php endif; ?>
--- core/app/view/updateproduct-view.php
+++ core/app/view/updateproduct-view.php
@@ -1,52 +1,59 @@
 <?php
 
 if(count($_POST)>0){
-	$product = ProductData::getById($_POST["product_id"]);
+	// Obtener el producto original primero
+	$original_product = ProductData::getById($_POST["product_id"]);
+	if(!$original_product) {
+		setcookie("prdupd_error", "No se encontró el producto a actualizar", time() + 3600, "/");
+		header("Location: index.php?view=inventary");
+		exit;
+	}
 
-	$product->barcode = $_POST["barcode"];
-	$product->name = $_POST["name"];
-	$product->price_in = $_POST["price_in"];
-	$product->price_out = $_POST["price_out"];
-	$product->unit = $_POST["unit"];
-
-	$product->description = $_POST["description"];
-	$product->presentation = $_POST["presentation"];
-	$product->inventary_min = $_POST["inventary_min"];
-	$category_id="NULL";
-	if($_POST["category_id"]!=""){ $category_id=$_POST["category_id"];}
-
-	$is_active=0;
-	if(isset($_POST["is_active"])){ $is_active=1;}
-
-	$product->is_active=$is_active;
-	$product->category_id=$category_id;
-
-	// Manejar tipo_jersey con valor por defecto
-	$product->jersey_type = isset($_POST["tipo_jersey"]) ? $_POST["tipo_jersey"] : "";
-
-	// Validar duplicados según la categoría
-	$category = CategoryData::getById($category_id);
-	$categoryName = $category ? strtolower(trim($category->name)) : '';
+	// Verificar si hay cambios en los campos críticos
+	$category_id = $_POST["category_id"] != "" ? $_POST["category_id"] : "NULL";
+	$new_jersey_type = isset($_POST["tipo_jersey"]) ? $_POST["tipo_jersey"] : "";
 	
-	if($categoryName === 'jersey' && !empty($product->jersey_type)) {
-		// Para jerseys, verificar nombre, categoría y tipo de jersey
-		$existing_product = ProductData::existsByNameAndCategory($product->name, $category_id, $product->jersey_type);
-		if($existing_product && $existing_product->id != $product->id) {
-			setcookie("prdupd_error", "Ya existe un jersey de tipo '{$product->jersey_type}' con el nombre '{$product->name}'", time() + 3600, "/");
-			header("Location: index.php?view=editproduct&id=".$product->id);
-			exit;
-		}
-	} else if($category_id != "NULL") {
-		// Para otras categorías, verificar solo nombre y categoría
-		$existing_product = ProductData::existsByNameAndCategory($product->name, $category_id);
-		if($existing_product && $existing_product->id != $product->id) {
-			setcookie("prdupd_error", "Ya existe un producto con el nombre '{$product->name}' en la categoría '$categoryName'", time() + 3600, "/");
-			header("Location: index.php?view=editproduct&id=".$product->id);
-			exit;
+	$values_changed = (
+		$_POST["name"] !== $original_product->name || 
+		$category_id !== $original_product->category_id ||
+		$new_jersey_type !== $original_product->jersey_type
+	);
+
+	// Validar duplicados solo si cambiaron valores críticos
+	if($values_changed) {
+		$category = CategoryData::getById($category_id);
+		$categoryName = $category ? strtolower(trim($category->name)) : '';
+		
+		if($categoryName === 'jersey' && !empty($new_jersey_type)) {
+			$existing_product = ProductData::existsByNameAndCategory($_POST["name"], $category_id, $new_jersey_type);
+			if($existing_product && $existing_product->id != $original_product->id) {
+				setcookie("prdupd_error", "Ya existe un jersey de tipo '{$new_jersey_type}' con el nombre '{$_POST["name"]}'", time() + 3600, "/");
+				header("Location: index.php?view=editproduct&id=".$original_product->id);
+				exit;
+			}
+		} else if($category_id != "NULL") {
+			$existing_product = ProductData::existsByNameAndCategory($_POST["name"], $category_id);
+			if($existing_product && $existing_product->id != $original_product->id) {
+				setcookie("prdupd_error", "Ya existe un producto con el nombre '{$_POST["name"]}' en la categoría '$categoryName'", time() + 3600, "/");
+				header("Location: index.php?view=editproduct&id=".$original_product->id);
+				exit;
+			}
 		}
 	}
 
-	$product->user_id = $_SESSION["user_id"];
+	// Actualizar los valores del producto
+	$original_product->barcode = $_POST["barcode"];
+	$original_product->name = $_POST["name"];
+	$original_product->price_in = $_POST["price_in"];
+	$original_product->price_out = $_POST["price_out"];
+	$original_product->unit = $_POST["unit"];
+	$original_product->description = $_POST["description"];
+	$original_product->presentation = $_POST["presentation"];
+	$original_product->inventary_min = $_POST["inventary_min"];
+	$original_product->category_id = $category_id;
+	$original_product->jersey_type = $new_jersey_type;
+	$original_product->is_active = isset($_POST["is_active"]) ? 1 : 0;
+	$original_product->user_id = $_SESSION["user_id"];
 
 	// Si hay una imagen nueva
 	if(isset($_FILES["image"]) && $_FILES["image"]["error"] == 0){
@@ -54,23 +61,20 @@ if(count($_POST)>0){
 		if($image->uploaded){
 			$image->Process("storage/products/");
 			if($image->processed){
-				$product->image = $image->file_dst_name;
-				$product->update_with_image(); // Usar el método correcto para actualizar con imagen
+				$original_product->image = $image->file_dst_name;
+				$original_product->update_with_image();
 			} else {
-				// Error procesando la imagen
 				setcookie("prdupd_error", "Error al procesar la imagen");
-				header("Location: index.php?view=editproduct&id=".$product->id);
+				header("Location: index.php?view=editproduct&id=".$original_product->id);
 				exit;
 			}
 		} else {
-			// Error subiendo la imagen
 			setcookie("prdupd_error", "Error al subir la imagen");
-			header("Location: index.php?view=editproduct&id=".$product->id);
+			header("Location: index.php?view=editproduct&id=".$original_product->id);
 			exit;
 		}
 	} else {
-		// No hay imagen nueva, actualizar sin imagen
-		$product->update();
+		$original_product->update();
 	}
 
 	setcookie("prdupd","true");
@@ -85,7 +89,6 @@ if(count($_POST)>0){
 		}
 	}
 
-	// Redirigir con mensaje de éxito
 	header("Location: " . $redirect_url);
 	exit;
 }
